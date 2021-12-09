4e7bfdf06c3e43ed947e5fec20b00bfef117cf50
Kamil678 <kamilpigulak@interia.pl>
2021-12-09 15:31:38 +0100

    Updates

---
+++ precache-manifest.c1dc1bae67460b1da5f47978d3fb89ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e913047454fa97ab269",
+    "url": "/WeatherApp/static/css/main.b873ee96.chunk.css"
+  },
+  {
+    "revision": "2e913047454fa97ab269",
+    "url": "/WeatherApp/static/js/main.2e913047.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/WeatherApp/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "d9b2d03dc90505701265782df1138739",
+    "url": "/WeatherApp/static/media/sky-g6d0a50f64_1280.d9b2d03d.png"
+  },
+  {
+    "revision": "aa5ce1999299bce5d3bb0aeeab964b7d",
+    "url": "/WeatherApp/index.html"
+  }
+];
